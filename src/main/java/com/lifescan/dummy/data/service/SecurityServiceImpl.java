a425808ee1cde28bb02f49b9972bc9bd80c62516
CIANDT\fjansen <felipe.jansen@ciandt.com>
2021-10-28 16:13:57 -0300

    [OTRW-23507] Generating default values when user doesnt inform preset.

--- src/main/java/com/lifescan/dummy/data/service/SecurityServiceImpl.java
+++ src/main/java/com/lifescan/dummy/data/service/SecurityServiceImpl.java
@@ -28,7 +28,7 @@ public class SecurityServiceImpl implements SecurityService {
   @Override
   public String doLogin(Login user) {
     return securityServiceCore
-        .authenticate("neivajansen.felipe@gmail.com", "9876374597aA")
+        .authenticate(user.getEmail(), user.getPassword())
         .getResult()
         .getToken();
   }
